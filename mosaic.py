4742e3f7f31ea6a38b5a462548178d7fa4784354
Forests <99336909+forests0@users.noreply.github.com>
2022-12-28 08:35:14 +0900

    [refactoring] : waitkey 변수 추가

--- mosaic.py
+++ mosaic.py
@@ -3,7 +3,7 @@ import cv2
 
 xml = 'haarcascades/haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(xml)
-
+WAIT_DELAY = 20
 cap = cv2.VideoCapture(0) 
 cap.set(3,2160) 
 cap.set(4,960) 
@@ -18,15 +18,13 @@ while(True):
     if len(faces):
       for (x,y,w,h) in faces:
         face_img = frame[y:y+h, x:x+w] 
-        face_img = cv2.resize(face_img, dsize=(0, 0), fx=0.08, fy=0.08) 
+        face_img = cv2.resize(face_img, dsize=(0, 0), fx=0.07, fy=0.07) 
         face_img = cv2.resize(face_img, (w, h), interpolation=cv2.INTER_AREA) 
         frame[y:y+h, x:x+w] = face_img 
 
-      
-
     cv2.imshow('result', frame)
         
-    k = cv2.waitKey(30) & 0xff
+    k = cv2.waitKey(WAIT_DELAY) & 0xff
     if k == 27: 
         break
 
